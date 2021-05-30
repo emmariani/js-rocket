6a3f932e7b8765f030ee568ec6f21ca1eaa1ff59
emmariani <44271114+emmariani@users.noreply.github.com>
2021-05-30 11:36:09 +0200

    car stuff

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 var car = {
-			make: 'VW',
-			type: 'Polo',
-			Color: 'blue',
-			isTurnedOn: false,
-			numberOfWheels: 4,
-			seats: [
-				'seat 1',
-				'seat 2', 
-				'seat 3',
-				'seat 4'
-			],
-			turnOn: function () {
-				 this.isTurnedOn = true;
-			},
-			fly: function () {
-				alert('fly');
-			},
-			switchCar: function (isOn) {
-				console.log('turn car'+isOn)
-				if (isOn == true) {
-					this.isTurnedOn = true;
-				} else {
-					this.isTurnedOn = false;
-				}
+		make: 'Fiat',
+		type: 'Panda',
+		color: 'pink',
+		isTurnedOn: false,
+		numberOfWheels: 4,
+		seats: [
+			'seat 1', 
+			'seat 2', 
+			'seat 3', 
+			'seat 4'
+		],
+		TurnOn: function () {
+			this.isTurnedOn = true;
+		},
+		fly: function () {
+			alert('Cars can not fly you dummy');
+		},
+		switchCar: function (isOn) {
+			console.log ('turn car' +isOn);
+			if (isOn == true) {
+				this.isTurnedOn = true;
+			} else {
+				this.isTurnedOn = false;
 			}
-		};
+		}
+	};
 
-		console.log('hello world');
+	console.log('Final count down for lift off');
